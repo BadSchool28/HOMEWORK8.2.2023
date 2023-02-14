d728ad7bee5513680553446b1d7285e65a48e786
BadSchool28 <70092216+BadSchool28@users.noreply.github.com>
2023-02-14 22:16:18 +0700

    da hoan thanh

---
+++ SongList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongList
+{
+    class Song
+    {
+        public String TypeList { get; set; }
+        public String Name { get; set; }
+        public String Time { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding= Encoding.UTF8;
+            int numSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+            for (int i = 0; i<numSongs; i++)
+            {
+                String[] data = Console.ReadLine().Split('_');
+
+                String type = data[0];
+                String name = data[1];
+                String time = data[2];
+
+                Song song = new Song();
+
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+            }
+
+            string typelist = Console.ReadLine();
+            Console.WriteLine("***********ĐÂY LÀ DANH SÁCH NHẠC***********");
+            if(typelist == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == typelist)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lop/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lop
+{
+
+    internal class Student
+    {
+        int gpa;
+
+        public int Gpa
+        {
+            get { return gpa; }
+            set
+            {
+                gpa= value;
+            }
+        }
+        public void PrintGPA()
+        {
+            Console.WriteLine($"GPA: {gpa}");
+        }
+
+        
+    }
+    internal class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                    age = value;
+                else
+                    throw new Exception("tuoi khong hop le");
+            }
+        }
+        public void PrintPerson()
+        {
+            Console.WriteLine($"ten: {name}, tuoi : {age}");
+        }
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Student student1 = new Student();
+            person1.Name = "lok";
+            try
+            {
+                person1.Name = "Huy";
+                person1.Age = -3;
+                student1.Gpa = -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            person1.PrintPerson();
+            student1.PrintGPA();
+            Console.ReadLine();
+
+        }
+    }
+}
